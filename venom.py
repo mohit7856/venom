62c0ccbc34c1c50d17802795ab02f197237a88d0
mohit7856 <mohitraj00029@gmail.com>
2024-09-03 22:18:57 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -7,10 +7,10 @@ import certifi
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-TOKEN = '7527525916:AAH8lDrN62lbVBJSf4QD-fnv-EUdfpnR_zc'
+TOKEN = '7407497547:AAHyEtzp62osaCOtDt1E1wJfug0ZexrcLbY'
 MONGO_URI = 'mongodb+srv://VENOMxCRAZY:CRAZYxVENOM@cluster0.ythilmw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tlsAllowInvalidCertificates=true'
-CHANNEL_ID = -1002224570220
-ADMIN_IDS = [5588464519]
+CHANNEL_ID = -1002221813382
+ADMIN_IDS = [1825334364]
 
 
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
